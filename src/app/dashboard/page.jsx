f4788861d159549c25f5a9cfa67e6a7776eae9e7
ruhsbahpaunikar14 <ruhsbahpaunikar14@gmial.com>
2024-03-30 01:18:55 +0530

    Your commit message here

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,10 @@
 "use client"
+import { useSession } from "next-auth/react";
 import styles from "./page.module.css"
 import React, { useEffect, useState } from 'react'
+import useSWR from "swr"
 
-const Dashboard = () => {
+/* const Dashboard = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -24,5 +26,13 @@ const Dashboard = () => {
     </div>
   )
 }
-
+ */
+const Dashboard = () => {
+const session = useSession()
+console.log(session)
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts${id}")
+console.log(data)
+return <div className= {styles.container}>Dashboard</div>
+};
 export default Dashboard
